c2eb7926377e61586a838a6e1d9316806dbcd7fb
Anselm Schüler <mail@anselmschueler.com>
2023-08-23 18:52:20 +0200

    Write simple OsStrExt impls & add Nix flake & direnv envrc

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix.url = "github:nix-community/fenix";
+  };
+  outputs = { self, nixpkgs, flake-utils, fenix }: flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with fenix.packages.${system}; [(combine [
+          stable.toolchain
+          targets.wasm32-wasi.stable.rust-std
+          targets.x86_64-unknown-linux-gnu.stable.rust-std
+          targets.x86_64-pc-windows-gnu.stable.rust-std
+          targets.x86_64-pc-windows-msvc.stable.rust-std
+        ])];
+      };
+    });
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,43 @@
-use std::ffi::OsStr;
+#[cfg(target_os = "wasi")]
+use std::os::wasi::ffi::OsStrExt;
+
+#[cfg(target_family = "unix")]
+use std::os::unix::ffi::OsStrExt;
+
+#[cfg(target_family = "windows")]
+use std::os::windows::ffi::OsStrExt;
+
+use std::ffi::{OsStr, OsString};
 
 #[cfg(target_family = "windows")]
 pub struct OsStrItem(u16);
 
-#[cfg(any(target_family = "wasm", target_family = "unix"))]
+#[cfg(any(target_os = "wasi", target_family = "unix"))]
 #[derive(Clone, Copy)]
 pub struct OsStrItem(u8);
 
 pub trait OsStrManip {
     fn iter_items<'a>(&'a self) -> OsStrItems<'a>;
-    fn contains<'a, P: OsPattern<'a>>(&'a self, pat: P) -> bool;
+}
+
+#[cfg(any(target_os = "wasi", target_family = "unix"))]
+impl OsStrManip for OsStr {
+    fn iter_items<'a>(&'a self) -> OsStrItems<'a> {
+        OsStrItems(self.as_bytes().iter())
+    }
+}
+
+#[cfg(target_family = "windows")]
+impl OsStrManip for OsStr {
+    fn iter_items<'a>(&'a self) -> OsStrItems<'a> {
+        OsStrItems(self.encode_wide())
+    }
 }
 
 #[cfg(target_family = "windows")]
 pub struct OsStrItems<'a>(std::os::windows::ffi::EncodeWide<'a>);
 
-#[cfg(any(target_family = "wasm", target_family = "unix"))]
+#[cfg(any(target_os = "wasi", target_family = "unix"))]
 pub struct OsStrItems<'a>(std::slice::Iter<'a, u8>);
 
 #[cfg(target_family = "windows")]
@@ -29,7 +51,7 @@ impl<'a> Iterator for OsStrItems<'a> {
     }
 }
 
-#[cfg(any(target_family = "wasm", target_family = "unix"))]
+#[cfg(any(target_os = "wasi", target_family = "unix"))]
 impl<'a> Iterator for OsStrItems<'a> {
     type Item = OsStrItem;
     fn next(&mut self) -> Option<Self::Item> {
@@ -40,6 +62,59 @@ impl<'a> Iterator for OsStrItems<'a> {
     }
 }
 
-pub trait OsPattern<'a> {
-    fn matches(&self, haystack: &'a OsStr) -> Option<usize>;
+pub trait OsPattern<'a>: Sized {
+    type Searcher: OsSearcher<'a>;
+
+    fn into_searcher(self, haystack: &OsStr) -> Self::Searcher;
+
+    fn is_contained_in(self, haystack: &OsStr) -> bool {
+        todo!()
+    }
+    fn is_prefix_of(self, haystack: &OsStr) -> bool {
+        todo!()
+    }
+    fn is_suffix_of(self, haystack: &OsStr) -> bool
+    where
+        Self::Searcher: OsReverseSearcher<'a>,
+    {
+        todo!()
+    }
+    fn strip_prefix_of(self, haystack: &OsStr) -> OsString {
+        todo!()
+    }
+    fn strip_suffix_of(self, haystack: &OsStr) -> OsString
+    where
+        Self::Searcher: OsReverseSearcher<'a>,
+    {
+        todo!()
+    }
+}
+
+pub trait OsSearcher<'a> {
+    fn haystack(&self) -> &'a [OsStrItem];
+    fn next(&mut self) -> OsSearchStep;
+
+    fn next_match(&mut self) -> Option<(usize, usize)> {
+        todo!()
+    }
+    fn next_reject(&mut self) -> Option<(usize, usize)> {
+        todo!()
+    }
+}
+
+pub trait OsReverseSearcher<'a>: OsSearcher<'a> {
+    fn next_back(&mut self) -> OsSearchStep;
+
+    fn next_match_back(&mut self) -> Option<(usize, usize)> {
+        todo!()
+    }
+    fn next_reject_back(&mut self) -> Option<(usize, usize)> {
+        todo!()
+    }
+}
+
+pub enum OsSearchStep {
+    Match(usize, usize),
+    Reject(usize, usize),
+    Done,
 }
